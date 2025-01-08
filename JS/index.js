e0f4b5e326d5f98f13182c29aab982b2077f13e4
kkphoenix12 <singhkunal16492@gmail.com>
2025-01-08 21:58:52 +0530

    login updated

--- JS/index.js
+++ JS/index.js
@@ -42,5 +42,45 @@ function getBooks()
 { 
     return JSON.parse(localStorage.getItem("books")) || [];
 }
+function popup(){
+    console.log("press");
+    var loginForm = document.getElementById("container2");
+    if (loginForm.style.display === "none") {
+      loginForm.style.display = "flex"; // Show the form
+    // } else {
+    //   loginForm.style.display = "none"; // Hide the form
+    // }
+  }
+}
+
+
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+
+    let validUsername = "admin"
+    let validPassword = "admin@12345"
+
+    function validatation(){
+      
+        if(username.value === ""){
+            alert('username cannot be empty');
+            return false;
+        
+        }   
+        if(password.value === ""){
+            alert('password cannot be empty');
+            return false;
+        }
+        
+        if(username.value === validUsername && password.value === validPassword){
+            alert('login successful');
+            window.open('../addBook.html')
+            return true;
+        }
+        else{
+            alert('login failed');
+            return false;
+        }
+    }
  displayBooks();
 
